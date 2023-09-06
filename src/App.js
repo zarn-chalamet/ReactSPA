5811cbf6e680ad4f3a2849f237c9b2cddafd5fa2
zarn_chalamet <6531503195@lamduan.mfu.ac.th>
2023-09-06 23:55:23 +0700

    useState

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 import image1 from "./assets/reactCourseLogo.png";
 
 function App() {
-  let name = "Zarni Tun"
+  let [name,setName] = useState("Zarni Tun");//[getter,setterFun]
+
+  let changeName = () => {
+    setName("Zarn");
+    console.log(name);
+  }
+  
   return (
     <div className='app'>
+      
+    
       <h1>Hello {name}</h1>
+      <button onClick={changeName}>
+        Change Name
+      </button>
       {/*relative url */}
       {/*<img src="/reactCourseLogo.png"></img>*/}
 
