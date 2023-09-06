b19f7907b278711aa3139b351aff045f3e264b19
zarn_chalamet <6531503195@lamduan.mfu.ac.th>
2023-09-06 23:10:39 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import image1 from "./assets/reactCourseLogo.png";
 
 function App() {
+  let name = "Zarni Tun"
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app'>
+      <h1>Hello {name}</h1>
+      {/*relative url */}
+      {/*<img src="/reactCourseLogo.png"></img>*/}
+
+      {/*import*/}
+      {/*<img src={image1}></img> */}
+
     </div>
   );
 }
