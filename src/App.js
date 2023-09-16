5ea260abd2fbb33914b0bee8f60bc97906b206c1
zarn_chalamet <6531503195@lamduan.mfu.ac.th>
2023-09-16 22:20:46 +0700

    post and setPost

--- src/App.js
+++ src/App.js
@@ -1,30 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
-import { useState } from 'react';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
 import image1 from "./assets/reactCourseLogo.png";
 
 function App() {
-  let [name,setName] = useState("Zarni Tun");//[getter,setterFun]
+  let [name, setName] = useState("Zarni Tun"); //[getter,setterFun]
+
+  let [posts, setPosts] = useState([
+    {
+      id: 1,
+      title: "First Post",
+    },
+    {
+      id: 2,
+      title: "Second Post",
+    },
+    {
+      id: 3,
+      title: "Third Post",
+    },
+  ]);
 
   let changeName = () => {
     setName("Zarn");
     console.log(name);
-  }
-  
+  };
+
   return (
-    <div className='app'>
-      
-    
+    <div className="app">
       <h1>Hello {name}</h1>
-      <button onClick={changeName}>
-        Change Name
-      </button>
+      <button onClick={changeName}>Change Name</button>
       {/*relative url */}
       {/*<img src="/reactCourseLogo.png"></img>*/}
 
       {/*import*/}
       {/*<img src={image1}></img> */}
 
+      <h2>Post</h2>
+      <ul>
+        {posts.map((post) => (
+          <li>{post.title}</li>
+        ))}
+      </ul>
     </div>
   );
 }
